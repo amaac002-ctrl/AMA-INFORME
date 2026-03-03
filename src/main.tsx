3a52aceb90df852948b295b8fb926ee2433953f9
Fran <fran@example.com>
2026-03-03 10:10:40 +0000

    Correccion de rutas para Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log("React: Iniciando montaje de la aplicación...");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
